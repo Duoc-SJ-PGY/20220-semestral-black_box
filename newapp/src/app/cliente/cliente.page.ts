8a654de05e70f7ed58898d4e56c62a40a5fa031a
Brian Quilinan <81407102+Brian150798@users.noreply.github.com>
2022-10-26 16:49:09 -0300

    actulizacion
    actual

---
+++ newapp/src/app/cliente/cliente.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.page.html',
+  styleUrls: ['./cliente.page.scss'],
+})
+export class ClientePage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  GoToHome(){
+    this.router.navigate(['/home'])
+
+
+  }
+
+}
